fc06d8e6fb4ed1e19185bea90f439cc7fd04e2b8
mohamedali0122 <mohamedalhelo909@gmail.com>
2024-06-05 19:45:41 +0300

    is done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
+import Pharamacypage from "./components/Pharamacypage";
 function App() {
   return (
     <div>
-      <NavBar />
+      <Pharamacypage />
       <Routes></Routes>
     </div>
   );
